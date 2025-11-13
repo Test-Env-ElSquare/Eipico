678e9835bfc20a19718054c7b1d8201d2c7f440b
ayamedhat229 <ayamedhat773@gmail.com>
2025-11-13 16:48:08 +0200

    production-link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  url: 'http://192.168.1.25:21576/',
+  url: 'http://10.1.1.240/Eipico/',
   // url: 'http://10.1.1.240/eipico/', server epicio
 
-  baseUrl: 'http://192.168.1.47:5001/', // realTime
+  // baseUrl: 'http://192.168.1.47:5001/', // realTime
   eipicoItInterfaceURL: 'http://10.1.1.240/eipico_IT_Interface/',
 
   // url:'http://196.219.184.42:21576/'
